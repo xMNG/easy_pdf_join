99a153dd68149a54a1814f33a90e87e34413e6d3
MNG <ng.michael.m@gmail.com>
2018-12-11 19:35:29 -0500

    initial commit

---
+++ pdf_join.py
@@ -0,0 +1,30 @@
+#! python3
+# pdf_join.py - Joins entire pdfs together easily because no open source software exists (that I can find).
+# Usage: python pdf_join.py --output=new_pdf.pdf --merge "pdf to merge 1" "pdf to merge 2" ...
+import argparse
+from PyPDF2 import PdfFileMerger
+
+parser = argparse.ArgumentParser()
+# need a filename for output
+parser.add_argument('--output_file', action='store', default='new_pdf.pdf', help='Provide filename for output pdf')
+# need a list for documents to merge, in order
+parser.add_argument('--merge', action='append', nargs='+', help='Provide filenames for pdfs to merge (must be in current working directory')
+# parse args
+args = parser.parse_args()
+
+# pdf list as a constant
+PDFS = args.merge
+PDFS = [item for sublist in PDFS for item in sublist]
+
+# init the merger class
+merger = PdfFileMerger()
+
+# add all pdfs to the merger class
+for pdf in PDFS:
+    merger.append(pdf)
+    print(f'Merging {pdf}...')
+
+with open(args.output_file, 'wb') as f:
+    merger.write(f)
+
+print('Merge complete.')
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Easy PDF Join 
+Easy command line pdf joining, because I couldn't find an open source PDF joiner and I don't want to pay for acrobat pro
+
+<h2>REQUIREMENTS</h2>
+- Python 3.6+
+- PyPDF2 1.26.0+
+ 
+<h2> How to Use </h2>  
+
+- Enter into command line:
+```
+python pdf_join.py --output=new_pdf.pdf --merge "pdf to merge 1" "pdf to merge 2" ...
+```
